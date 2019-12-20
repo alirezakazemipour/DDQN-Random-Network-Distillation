033a37143cd60b3fb74f2b6b5527258f32cbec2f
alirezaka <emailmailykazemipour@gmail.com>
2019-12-20 16:08:39 +0330

    Implement Sampling

--- memory.py
+++ memory.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sumtree import SumTree
+
 
 class Memory:
 
@@ -7,18 +9,20 @@ class Memory:
     beta = 0.4
     beta_growth_rate = 0.001
 
-    def __init__(self):
-        pass
+    def __init__(self, mem_size):
+        self.mem_size = mem_size
+        self.tree = SumTree(mem_size)
 
     def sample(self):
         pass
 
-    def add(self):
-        pass
+    def add(self, error, transition):
+        p = self.get_priority(error)
+        self.tree.add(p, transition)
 
     def get_priority(self, error):
         p = (np.abs(error) + self.epsilon) ** self.alpha
         return p
 
-    def update_tree(self):
-        pass
+    def update_tree(self, idx, p):
+        self.tree.update(idx, p)
--- sumtree.py
+++ sumtree.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 class SumTree:
+    data_pointer = 0
+
     def __init__(self, transition_size):
 
         self.transition_size = transition_size
@@ -20,5 +22,36 @@ class SumTree:
         if parent_idx != 0:
             self.propagate(parent_idx, delta_p)
 
-    def update(self):
+    def update(self, index, p):
+
+        delta_p = p - self.tree[index]
+        self.tree[index] = p
+
+        self.propagate(index, delta_p)
+
+    def add(self, p, data):
+        idx = self.data_pointer + self.transition_size -1
+
+        self.data[self.data_pointer] = data
+        self.update(idx, p)
+
+        self.data_pointer += 1
+        if self.data_pointer >= self.transition_size:
+            self.data_pointer = 0
+
+    def get_leaf_index(self, parent_idx, s):
+
+        left_child = 2 * parent_idx + 1
+        right_child = parent_idx + 1
+
+        if left_child >= len(self.tree):
+            return parent_idx
+
+        if s <= self.tree[left_child]:
+            return self.get_leaf_index(left_child, s)
+        else:
+            s -= self.tree[left_child]
+            return self.get_leaf_index(right_child, s)
+
+    def get(self):
         pass
